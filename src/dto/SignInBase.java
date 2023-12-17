f379d37f7a0af873637358a04ae8fadf0c89c9d1
mayar <100476119+mayar1711@users.noreply.github.com>
2023-12-17 10:43:30 +0200

    create sign in class

---
+++ src/dto/SignInBase.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dto;
+
+/**
+ *
+ * @author mayar
+ */
+public class SignInBase {
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public SignInBase(String name, String password, String status) {
+        this.name = name;
+        this.password = password;
+        this.status = status;
+    }
+    private String name;
+    private String password;
+    private String status;
+}
